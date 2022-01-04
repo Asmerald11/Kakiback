124311e3481885707f3f12385a750ba2b2e369c7
Santi Morelli <santimore10@gmail.com>
2022-01-04 12:43:43 -0300

    Seguimos probando

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const { dbConnection } = require('./database/config');
 const cors = require('cors');
 const compression = require('compression');
 const Usuario = require('./models/Usuario');
-const path = require('path')
 
 const app = express();
 
@@ -14,12 +13,7 @@ dbConnection();
 app.use(compression());
 app.use(cors());
 app.use(express.json());
-
-/* app.use(express.static(path.join(__dirname, '../client/build'))) */
 app.use('/api/auth', require('./routes/auth'));
-/* app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '../client/build/index.html'))
-}) */
 
 app.listen(9000, () => {
     console.log(`Servidor corriendo en el puerto 9000`);
